72208e8088a2555ec91cb9fb438e757470edfc61
Riccardo Foti <riccardofoti97@gmail.com>
2024-09-02 17:23:42 +0100

    Change variable names

--- src/index.py
+++ src/index.py
@@ -6,23 +6,23 @@ import os
 
 load_dotenv()
 
-apiKey = os.getenv('API_KEY')
+api_key = os.getenv('API_KEY')
 
-def getContent(searchTerm, reference, dateFrom = ""):
-    dateFrom = dateFrom.replace("date_from", "from-date")
-    url = f'https://content.guardianapis.com/search?q={searchTerm}&{dateFrom}&api-key={apiKey}'
+def getContent(search_term, reference, date_from = ""):
+    date_from = date_from.replace("date_from", "from-date")
+    url = f'https://content.guardianapis.com/search?q={search_term}&{date_from}&api-key={api_key}'
     response = requests.get(url)
     articles = response.json()["response"]["results"]
 
-    myArticles = {reference :[]}
+    my_articles = {reference :[]}
     for index,item in enumerate(articles):
         myArticle = {'webPublicationDate' : articles[index]['webPublicationDate'],
                     'webTitle' : articles[index]['webTitle'],
                     'webUrl' : articles[index]['webUrl']
                     }
-        myArticles[reference].append(myArticle)
+        my_articles[reference].append(myArticle)
 
-    print(json.dumps(myArticles))
+    print(json.dumps(my_articles))
 
 
 getContent("machine learning", "guardian_content", "date_from=2023-01-01")
